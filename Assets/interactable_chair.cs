a2ae9de9a0bcd48ab570319a054cad91b716027b
Julia9407 <julia.lueck@smail.inf.h-brs.de>
2025-12-15 22:31:25 +0100

    added_box_colliders_and_rewired_voice_control
    I added the new colliders and rewired the voice control for the new level scene

---
+++ Assets/interactable_chair.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ChairInteractable : Interactable
+{
+    public Transform sitPoint; // Position + Rotation fürs Hinsetzen
+
+    public override List<PlayerBehavior.Actions> StartInteraction()
+    {
+        var actions = new List<PlayerBehavior.Actions>();
+        actions.Add(new PlayerBehavior.Actions(
+            "sit down",
+            "Sit",
+            gameObject.name
+        ));
+        return actions;
+    }
+
+    public override void PerformInteraction(PlayerBehavior.Actions action, Inventory inventory)
+    {
+        if (action.verb == "Sit")
+        {
+            var player = FindObjectOfType<PlayerBehavior>();
+            player.SitDown(sitPoint);
+        }
+    }
+}
